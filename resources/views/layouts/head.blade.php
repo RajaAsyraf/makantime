daf61e061dadfb9a6218628f87fa13aa01480709
Raja Asyraf <md.asyraf@time.com.my>
2019-04-18 15:10:23 +0800

    Create dashboard page
    - Remove welcome page
    - Separate view files accordingly
    - Fix assets calling method

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,18 @@
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>{{ config('app.name', 'Makan Time') }}</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('theme/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('theme/modules/fontawesome/css/all.min.css') }}">
+
+  <!-- CSS Libraries -->
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('theme/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('theme/css/components.css') }}">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
