2c94efc4eda1a4086c9c9bb2215a24e665f52f17
Raja Asyraf <md.asyraf@time.com.my>
2019-05-08 17:13:59 +0800

    Fix invitation creation
    - Enable to send invitation to group

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Invitation;
+use App\InvitationUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class InvitationController extends Controller
@@ -77,9 +79,30 @@ class InvitationController extends Controller
             'time' => 'required',
             'date' => 'required',
         ]);
-        return json_encode($validatedData);
-        // TODO: validate input
+
+        $user = Auth::user();
+        $groupUser = $user->groupUsers()->where('group_id', $validatedData['group_id'])->first();
+        $group = $groupUser->group()->first();
+        $groupMembers = $group->groupUsers()->get();
+        $restaurant = $group->restaurants()->where('restaurant_id', $validatedData['restaurant_id'])->first();
+        $appointment_date = date('Y-m-d H:i:s', strtotime($validatedData['date'].$validatedData['time'].':00'));
+        
+        DB::transaction(function () use ($validatedData, $group, $groupMembers, $restaurant, $appointment_date) {
+            $invitation = Invitation::create([
+                'group_id' => $group->id,
+                'restaurant_id' => $restaurant->id,
+                'appointment_at' => $appointment_date,
+            ]);
+            foreach ($groupMembers as $member) {
+                $invitationUsers = InvitationUser::create([
+                    'invitation_id' => $invitation->id,
+                    'user_id' => $member->id
+                ]);
+            }
+        });
+
         // TODO: return errors
         // TODO: return notification
+        return json_encode($invitation);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,12 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/invitation', ['as' => 'invitation.show', 'uses' => 'InvitationController@show']);
     Route::post('/invitation/response/{invitation}', ['as' => 'invitation.storeResponse', 'uses' => 'InvitationController@storeResponse']);
     Route::get('/invitation/create', ['as' => 'invitation.create', 'uses' => 'InvitationController@create']);
-    Route::post('/invitation/store', ['as' => 'invitation.store', 'uses' => 'InvitationController@store']);
     Route::get('/group', ['as' => 'group.index', 'uses' => 'GroupController@index']);
     Route::get('/group/create', ['as' => 'group.create', 'uses' => 'GroupController@create']);
     Route::post('/group/store', ['as' => 'group.store', 'uses' => 'GroupController@store']);
     Route::get('/group/{group}', ['as' => 'group.show', 'uses' => 'GroupController@show']);
     // TODO: Create middleware to check only for admin group can proceed with this routes
+    Route::post('/invitation/store', ['as' => 'invitation.store', 'uses' => 'InvitationController@store']);
     Route::get('/group/{group}/invite', ['as' => 'group.invite', 'uses' => 'GroupController@invite']);
     Route::post('/group/{group}/invite/store', ['as' => 'group.invite.store', 'uses' => 'GroupController@storeInvitation']);
 });
